b3f1d1e481da049607b6108b9c0de2e26fe8351c
pegadalg <paulovitor.freitas18@gmail.com>
2024-03-24 19:49:01 -0300

    Adicionando Console.Clear()

--- TrabalhoTabuada/Program.cs
+++ TrabalhoTabuada/Program.cs
@@ -6,16 +6,19 @@ do
 {
     try
     {
+        
         Console.WriteLine("\nPressione 0 para sair \nPressione 1 para somar números \nPressione 2 para converter medida ( Metros x Milímetros) \nPressione 3 para exibir as tabuadas do 1 ao 10\nPressione 4 para realizar tabuada de valor único");
         int escolha = int.Parse(Console.ReadLine());
         switch (escolha)
         {
             case 0:
-                Console.WriteLine("Você saiu!");
+                Console.Clear();
+                Console.WriteLine("Você saiu! Volte Sempre");
                 continua = false;
                 break;
-
+                
             case 1:
+                Console.Clear();
                 Console.WriteLine("Escolha dois números para somar: ");
                 float num1 = float.Parse(Console.ReadLine());
                 float num2 = float.Parse(Console.ReadLine());
@@ -24,6 +27,7 @@ do
                 break;
 
             case 2:
+                Console.Clear();
                 Console.WriteLine("Insira o valor em Metros que você deseja converter para milimetros: ");
                 float metros = float.Parse(Console.ReadLine());
 
@@ -31,24 +35,28 @@ do
                 break;
 
             case 3:
+                Console.Clear();
                 Console.WriteLine(ClasseTrabalho.Tabuada());
                 break;
 
             case 4:
+                Console.Clear();
                 Console.WriteLine("Escolha o número que deseja saber a tabuada");
                 float num = float.Parse(Console.ReadLine());
                 Console.WriteLine(ClasseTrabalho.Tabuada(num));
                 break;
 
             default:
+                Console.Clear();
                 Console.WriteLine("Valor não aceito");
                 break;
         }
+
     }
     catch (FormatException)
     {
         Console.WriteLine("Formato Inválido. Digite apenas números de 0 a 4");
     }
-
+    
 }
 while (continua == true);
